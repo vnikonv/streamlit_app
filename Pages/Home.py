8c276a98bce15f0afef13c06ca081a6e27fd7349
Nikon Veremeichik <thesecondnikon@gmail.com>
2024-11-02 21:18:17 +0500

    Sync

---
+++ Pages/Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+class Home:
+    def __init__(self):
+        pass
+    def app(self):
+        st.write("Home")
---
+++ Pages/Project3.py
@@ -0,0 +1,6 @@
+import streamlit as st
+class Project3:
+    def __init__(self):
+        pass
+    def app(self):
+        st.write("Project3")
