3d68d6d4b8e588e39448defe4f20e8f96aac45d5
Nikon Veremeichik <thesecondnikon@gmail.com>
2024-11-10 21:04:08 +0500

    Added page and navbar icon

--- App.py
+++ App.py
@@ -1,33 +1,51 @@
 import streamlit as st
 from Pages import Home, Project1, Project2, Project3
 from streamlit_navigation_bar import st_navbar as navbar
+from PIL import Image
 
-st.set_page_config(initial_sidebar_state="collapsed")
+image = Image.open("img/magic.png")
+st.set_page_config(initial_sidebar_state="collapsed", page_icon=image)
 
 pages = ["Home", "Project1", "Project2", "Project3"]
 
 styles = {
     "nav": {
         "background-color": "rgb(123, 209, 146)",
+        "display": "flex",
+        "justify-content": "center",
+        "font-size": "20px",
+        "color": "white",
     },
+
+    "img": {
+        "position": "absolute",
+        "left": "-20px",
+        "top": "4px",
+        "width": "100px",
+        "height": "40px",
+    },
+
     "div": {
         "max-width": "32rem",
     },
+
     "span": {
         "border-radius": "0.5rem",
         "color": "rgb(49, 51, 63)",
         "margin": "0 0.125rem",
         "padding": "0.4375rem 0.625rem",
     },
+
     "active": {
         "background-color": "rgba(105, 114, 255, 0.25)",
     },
+
     "hover": {
         "background-color": "rgba(255, 255, 255, 0.35)",
     }
 }
 
-page = navbar(pages, styles = styles)
+page = navbar(pages, logo_path="img/magic.svg", styles=styles)
 
 if page == "Home":
     Home.Home().app()
