3f811d195cbdec4310822f68ad7d194e3d25d3c6
Nikon Veremeichik <thesecondnikon@gmail.com>
2024-11-20 04:15:25 +0500

    Debug

--- Pages/Project2.py
+++ Pages/Project2.py
@@ -47,16 +47,16 @@ class Project2:
         elif source_option == "Local drive":
             file = st.file_uploader("Upload a video or a photo", type = ["mp4", "avi", "mov", "png", "jpg"])
             if file:
-                if file.type in ["mp4", "avi", "mov"]:
-                    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=Path(file.name).suffix)
-                    temp_file.write(file.read())
-                    video_url = temp_file.name
-                    st.video(video_url)
-                elif file.type in ["png", "jpeg"]:
+                # if file.type in ["mp4", "avi", "mov"]:
+                temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=Path(file.name).suffix)
+                temp_file.write(file.read())
+                video_url = temp_file.name
+                st.video(video_url)
+                """ elif file.type in ["png", "jpeg"]:
                     img_file = file
                     file_bytes = np.asarray(bytearray(img_file.read()), dtype=np.uint8)
                     frame = cv2.imdecode(file_bytes, 1)
-                    st.image(frame, channels="BGR")
+                    st.image(frame, channels="BGR") """
 
 
         elif source_option == "Web camera":
