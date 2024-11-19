d6948185d16533b95d8a235a9f6a73e82d470115
Nikon Veremeichik <thesecondnikon@gmail.com>
2024-11-20 04:28:31 +0500

    Reverted the option to upload photos from drive

--- Pages/Project2.py
+++ Pages/Project2.py
@@ -45,18 +45,12 @@ class Project2:
                     st.video(video_url)
 
         elif source_option == "Local drive":
-            file = st.file_uploader("Upload a video or a photo", type = ["mp4", "avi", "mov", "png", "jpg"])
-            if file:
-                # if file.type in ["mp4", "avi", "mov"]:
-                temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=Path(file.name).suffix)
-                temp_file.write(file.read())
+            video_file = st.file_uploader("Upload a video", type = ["mp4", "avi", "mov"])
+            if video_file:
+                temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=Path(video_file.name).suffix)
+                temp_file.write(video_file.read())
                 video_url = temp_file.name
                 st.video(video_url)
-                """ elif file.type in ["png", "jpeg"]:
-                    img_file = file
-                    file_bytes = np.asarray(bytearray(img_file.read()), dtype=np.uint8)
-                    frame = cv2.imdecode(file_bytes, 1)
-                    st.image(frame, channels="BGR") """
 
 
         elif source_option == "Web camera":
